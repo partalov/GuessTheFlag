0ee9b4cf77eaa1550cb70b35bcf26abe07e3be0b
partalov <partalov@hotmail.com>
2021-07-13 21:46:06 +0200

    Initial Commit

---
+++ P2.Guess The Flag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  P2.Guess The Flag
+//
+//  Created by Ilija Partalov on 13.7.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ P2.Guess The FlagTests/P2_Guess_The_FlagTests.swift
@@ -0,0 +1,33 @@
+//
+//  P2_Guess_The_FlagTests.swift
+//  P2.Guess The FlagTests
+//
+//  Created by Ilija Partalov on 13.7.21.
+//
+
+import XCTest
+@testable import P2_Guess_The_Flag
+
+class P2_Guess_The_FlagTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
